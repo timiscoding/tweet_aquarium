a20f28f9b7ad22d80b8c39f2e593a23b6983c0a9
Timothy Tsui <timiscoding@gmail.com>
2016-02-23 17:26:42 +1100

    Using postgresql instead of sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160125003200) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tweets", force: :cascade do |t|
     t.text     "post"
     t.integer  "user_id"
